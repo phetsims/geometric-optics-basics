945d5378ed3813fa408461161be08be0f69b63ec
pixelzoom <cmalley@pixelzoom.com>
2022-02-22 13:00:00 -0700

    default ?enable2F=true, ignore ?focalLengthControl, https://github.com/phetsims/geometric-optics-basics/issues/2

--- js/geometric-optics-basics-main.ts
+++ js/geometric-optics-basics-main.ts
@@ -7,12 +7,22 @@
  */
 
 import GOConstants from '../../geometric-optics/js/common/GOConstants.js';
+import GOGlobalOptions from '../../geometric-optics/js/common/GOGlobalOptions.js';
+import GOQueryParameters from '../../geometric-optics/js/common/GOQueryParameters.js';
 import LensScreen from '../../geometric-optics/js/lens/LensScreen.js';
 import MirrorScreen from '../../geometric-optics/js/mirror/MirrorScreen.js';
 import Sim from '../../joist/js/Sim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import geometricOpticsBasicsStrings from './geometricOpticsBasicsStrings.js';
+
+// ?focalLengthControl is ignored, and the 'direct' focal-length model is used.
+GOGlobalOptions.focalLengthControlTypeProperty.value = 'direct';
+
+// If enable2F query parameter was not in the URL, change the default.
+if ( !QueryStringMachine.containsKey( 'enable2F' ) ) {
+  GOQueryParameters.enable2F = true;
+}
 
 const simOptions = {
 
