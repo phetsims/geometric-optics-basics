9054d70b5290943a6acee447f8f9cd064ee33f48
Chris Malley <cmalley@pixelzoom.com>
2022-03-03 17:59:26 -0700

    add 2F feature to Options dialog, https://github.com/phetsims/geometric-optics/issues/356

--- js/geometric-optics-basics-main.ts
+++ js/geometric-optics-basics-main.ts
@@ -7,26 +7,21 @@
  */
 
 import GOGlobalOptions from '../../geometric-optics/js/common/GOGlobalOptions.js';
-import GOQueryParameters from '../../geometric-optics/js/common/GOQueryParameters.js';
 import GOSim from '../../geometric-optics/js/GOSim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
 import geometricOpticsBasicsStrings from './geometricOpticsBasicsStrings.js';
 
-// In Geometric Optics: Basics, we want the '2F Points' checkbox to be included in the control panel, and this is
-// controlled by the enable2F query parameter. If the enable2F query parameter was not in the URL, set it to true.
-// This must be done in geometric-optics-basics-main.ts so that it only affects this sim.
-if ( !QueryStringMachine.containsKey( 'enable2F' ) ) {
-  GOQueryParameters.enable2F = true;
-}
-
 simLauncher.launch( () => {
 
-  // In Geometric Optics: Basis, we want to use the 'direct' method of setting focal length. This puts a Focal Length
-  // control in the control panel, instead of Radius of Curvature and Index of Refraction controls. While the full
-  // version of the sim includes controls to change the model in the Options dialog, those controls will be omitted
-  // in this sim. So we ignore the focalLengthControl query parameter, and set this Property directly. Note that
-  // changing this Property must be done inside the callback to simLauncher.launch, after PhET-iO has been initialized.
+  // In Geometric Optics: Basis, we want to ignore the focalLengthControl query parameter, and use the 'direct' method
+  // of setting focal length.
   GOGlobalOptions.focalLengthModelTypeProperty.value = 'direct';
+
+  // In Geometric Optics: Basics, we want to respect the enable2F query parameter. But if that query parameter is
+  // not provided, then we enable this feature.
+  if ( !QueryStringMachine.containsKey( 'enable2F' ) ) {
+    GOGlobalOptions.enable2FProperty.value = true;
+  }
 
   const sim = new GOSim( geometricOpticsBasicsStrings[ 'geometric-optics-basics' ].title, {
     isBasicsVersion: true
