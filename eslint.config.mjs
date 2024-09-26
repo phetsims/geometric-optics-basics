9711f7fcdca329accdc0e7430d693c421df0acc9
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:25 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for geometric-optics-basics.
+ * ESLint configuration for geometric-optics-basics.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
